9ada71aa2e9dcc4b10b783fb96c50f79ba8f29b3
Tyler M <tmorse2222@gmail.com>
2022-09-29 21:05:11 -0700

    Removed redundant const in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
 // const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -32,6 +33,8 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening at http://localhost:3001'));
