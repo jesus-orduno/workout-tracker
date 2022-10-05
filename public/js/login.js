597808eab923c60f075bf82e833b5016a4b7381b
Tyler M <tmorse2222@gmail.com>
2022-10-04 17:08:06 -0700

    Added functionality to signup and login buttons

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
+      document.location.replace('/dashboard');
     } else {
       alert(response.statusText);
     }
@@ -25,7 +25,7 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
+  const username = document.querySelector('#name-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
@@ -37,8 +37,9 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
-    } else {
+      document.location.replace('/dashboard');
+    }
+    else {
       alert(response.statusText);
     }
   }
