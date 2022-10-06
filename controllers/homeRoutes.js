df42f190ca5dfb5650693e7edae19ffff456b6ad
Tyler M <tmorse2222@gmail.com>
2022-10-06 15:17:37 -0700

    Created functionality for deleteAccount.handlebars; full CRUD requirements met

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -32,12 +32,10 @@ router.get('/', withAuth, async (req, res) => {
 
 // The login page
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
-
+if (req.session.logged_in) {
+  res.redirect('/dashboard');
+  return;
+}
   res.render('login');
 });
 
